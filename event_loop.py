b85fc25b951b5dfc2ab5dc8966b7d7449cb34cb1
Havard Bjerke <hkfbjerke@gmail.com>
2016-05-28 14:39:37 +0200

    Adds spacebar input and quit event.

--- event_loop.py
+++ event_loop.py
@@ -7,16 +7,32 @@ class EventLoop:
         self.character = character.Character(screen)
 	self.reference_trace = trace.Trace()
 	self.trace_position = 0 # vertical position relative to center
+	self.clock = pygame.time.Clock()
+	self.done = False
+        self.step = 1 # the number of pixels to move the comparison trace per iteration
 
     def start(self):
-        while True:
+        while not self.done:
             self.run()
 
+        pygame.quit()
+
     def run(self):
-	self.screen.screen.fill((255, 255, 255))
+	self.clock.tick(10)
+
+        for event in pygame.event.get():
+            print(dir(event))
+            if event.type == pygame.QUIT:
+		self.done = True
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+		self.step = -self.step
+
+	self.screen.clear()
+
 	trace_position = (self.screen.v_center + self.trace_position) % self.screen.height
 	self.reference_trace.draw_line(self.screen.screen, self.screen.width, trace_position)
 	self.character.draw()
         pygame.display.flip()
-        self.trace_position += 1
-            
+
+        self.trace_position += self.step
+
