1fc48ab1342f962dbcba30372f45a0cb0e5ddf92
Havard Bjerke <hkfbjerke@gmail.com>
2016-05-29 13:04:46 +0200

    Adds segy->json converter.

---
+++ segy_reader.py
@@ -0,0 +1,17 @@
+import json
+from segpy import reader
+def main():
+	output_path = "data.json"
+	fh = open("line12.sgy", 'rb')
+	segy_reader = reader.create_reader(fh)
+	traces = []
+
+	for trace_index in segy_reader.trace_indexes():
+		trace = [float(a) for a in segy_reader.trace_samples(trace_index)]
+		traces.append(trace)
+
+	json.dump(traces, open(output_path, 'w'))
+
+if __name__ == "__main__":
+	main()
+
